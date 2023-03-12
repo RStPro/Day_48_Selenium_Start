5dc3ddf6dfe5f4a2f121764d8f45e326070b8ce3
RstProfessional <rsantos.professional@gmail.com>
2023-03-12 17:20:34 +0000

    configure selenium

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# we need to install webdriver-manager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+# we also need to import By
+from selenium.webdriver.common.by import By
 
 # evn path and variables
 dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-# we need to install webdriver-manager
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
-driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.amazon.es/-/pt/dp/B0B1YJRL9X/ref=sr_1_1?crid=2KC9ENME9RCD3&keywords=garmin+enduro+2&qid=1678548009&s=electronics&sprefix=garmin+enduro+%2Celectronics%2C97&sr=1-1')
 
-driver.get('https://www.amazon.es/pt')
+price = driver.find_element(By.CLASS_NAME, "a-price-whole")
+
+print(price.text)
+
+# driver.close() # closes a tab
+driver.quit() # quit the browser
