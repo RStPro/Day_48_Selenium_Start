f9a2048c626eb146a7a18d9da626952d27fb94c0
RstProfessional <rsantos.professional@gmail.com>
2023-03-12 16:42:28 +0000

    installed selenium and webdriver manager

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # evn path and variables
 dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-SELENIUM_PATH=os.getenv("SELENIUM_PATH")
+# we need to install webdriver-manager
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get('https://www.amazon.es/pt')
