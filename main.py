aee60281491557837a545393e69a77dcd8ea96ce
RstProfessional <rsantos.professional@gmail.com>
2023-03-13 21:08:59 +0000

    notes about converting to float

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 driver.get('https://www.garmin.com/pt-PT/p/854515')
-Xpath = '//*[@id="js__product__info__name"]'
-price = driver.find_element(By.XPATH, Xpath).get_attribute("innerHTML")
-
-print(price)
+Xpath = '//*[@id="js__product__price__main"]/span[1]'
+price = driver.find_element(By.XPATH, Xpath).get_attribute("innerHTML") # getting innerHTML to have the span text
+price_text = str((price).replace('&nbsp;', "")) # replacing white space by '' and converting to stirng
+price_number = float(price_text.replace(',', '.')) # replacing the ',' by '.' and convert to float
+print(price_number)
 
 # driver.close() # closes a tab
 driver.quit() # quit the browser
