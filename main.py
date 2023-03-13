96e978a7854dbcb8c922b6ef17746e96cec4abcd
RstProfessional <rsantos.professional@gmail.com>
2023-03-13 19:39:39 +0000

    ok amazon

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ driver = webdriver.Chrome(service=service)
 driver.get('https://www.amazon.es/-/pt/dp/B0B1YJRL9X/ref=sr_1_1?crid=2KC9ENME9RCD3&keywords=garmin+enduro+2&qid=1678548009&s=electronics&sprefix=garmin+enduro+%2Celectronics%2C97&sr=1-1')
 
 price = driver.find_element(By.CLASS_NAME, "a-price-whole")
-
 print(price.text)
 
 # driver.close() # closes a tab
